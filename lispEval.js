359311ad37a1942e94d1b71af343d5a216d2ffdf
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-10-17 12:26:54 +0530

    corrected if and boolean parser

--- lispEval.js
+++ lispEval.js
@@ -25,6 +25,8 @@ function symbolParser (input) {
   return null
 }
 
+// console.log(symbolParser('abc ) '))
+
 function atomParser (input) {
   return numberParser(input) || symbolParser(input) // an atom is a number or symbol
 }
@@ -39,7 +41,7 @@ function booleanParser (input) { // anything that is not false or #f is true in 
     const testArg = temp[0]
     input = temp[1].trim()
     const output = expressionEval(testArg)
-    if (output === 'false' || output === '#f') { return [false, input] }
+    if (output === false) { return [false, input] }
     else { return [true, input] }
   }
   console.log('invalid test consdition in if')
@@ -47,57 +49,40 @@ function booleanParser (input) { // anything that is not false or #f is true in 
 }
 
 function ifParser (input) { // testCondition passCase failCase)
-  const parsed = booleanParser(input)
+  let parsed = booleanParser(input)
   if (parsed === null) { console.log('can\'t parse testCondition'); return null }
-  const test = parsed[0]
+  const testCondition = parsed[0]
+  input = parsed[1].trim()
+
+  parsed = getListArg(input) || atomParser(input)
+  if (parsed === null) { return null }
+  const passCase = parsed[0]
   input = parsed[1]
 
-  if (test === false) {
-    let failCase
-    if (input[0] === '(') {
-      const temp = getListArg(input)
-      if (temp === null) { return null }
-      failCase = expressionEval(temp[0])
-      if (failCase === null) { return null }
-      input = temp[1].trim()
-    } else {
-      const temp = atomParser(input)
-      if (temp === null) { return null }
-      failCase = temp[0]
-      input = temp[1].trim()
-    }
+  parsed = getListArg(input) || atomParser(input)
+  if (parsed === null) { return null }
+  const failCase = parsed[0]
+  input = parsed[1]
+
+  if (testCondition === false) {
+    if (failCase[0] === '(') { return [expressionEval(failCase), input] }
     return [failCase, input]
   }
 
-  let passCase
-  if (input[0] === '(') {
-    const temp = getListArg(input)
-    if (temp === null) { return null }
-    passCase = expressionEval(temp[0])
-    if (passCase === null) { return null }
-    input = temp[1].trim()
-  } else {
-    const temp = atomParser(input)
-    if (temp === null) { return null }
-    passCase = temp[0]
-    input = temp[1].trim()
-  }
+  if (passCase[0] === '(') { return [expressionEval(passCase), input] }
   return [passCase, input]
-}
-
-// (define symbol exp)
-function defineParser (input) {
-
 }
 
 // special forms
 function formParser (op, input) {
+  // console.log(input, op)
   if (op === 'if') { return ifParser(input) }
-  if (op === 'define') { defineParser(input) }
+  // if (op === 'define') { defineParser(input) }
 }
 
 // seperating list expression argument
 function getListArg (input) { // input = (getThisArg) ()...())
+  if (input[0] !== '(') { return null }
   let count = 1
   let str = input.slice(1) // str = getThisArg) ()...()) , count = 1 // getThisArg is anything between ( and a matching )
   while (count !== 0) { // case when count !== 0?
@@ -109,7 +94,7 @@ function getListArg (input) { // input = (getThisArg) ()...())
       str = str.slice(1)
     } else { str = str.slice(1) }
   }
-  return [input.slice(0, input.length - str.length), str] // returns [listArg, rest of the string] // change to returning evaluated exp?
+  return [input.slice(0, input.length - str.length), str.trim()] // returns [listArg, rest of the string] // change to returning evaluated exp?
 }
 
 // get atom args and expression args
@@ -136,7 +121,7 @@ function getArgs (input) { // input = arg1, arg2,..) args are either atom or lis
 const specialForms = ['if', 'define', 'quote']
 
 function expressionEval (input, env = globalEnv) { // input = (op arg1 arg2 ...) arg is an atom or expression
-  if (input[0] !== '(') { return null }
+  if (input === null || input[0] !== '(') { return null }
   input = input.slice(1).trim() // input = op arg1 arg2 ...)
 
   const parsed = symbolParser(input) // operators(including special forms) are symbols
@@ -153,7 +138,16 @@ function expressionEval (input, env = globalEnv) { // input = (op arg1 arg2 ...)
   if (!Object.keys(env).includes(op)) { return null }
   const args = getArgs(input) // getArgs('(arg1 arg2...)') = [exp1Val, exp2Val ...] // input = arg1, arg2,...)
   if (args === null) { console.log('err: invalid expression'); return null }
+  // console.log('op:', op, 'args:', ...args, 'env[op](...args):', env[op](...args))
   return env[op](...args) // op([exp1Val, exp2Val ...])
+}
+// console.log(expressionEval('( > 30 45 )'))
+
+function evaluate (input) {
+  input = input.replaceAll('(', '( ').replaceAll(')', ' )')
+  console.log('input:', input)
+  if (input[0] !== '(') { return null }
+  return expressionEval(input)
 }
 
 let input
@@ -165,35 +159,39 @@ let input
 // // Math
 
 input = '(+ (+ 1 (- 1 1)) 2)' // 3
-console.log(expressionEval(input) === 3)
+console.log(evaluate(input) === 3)
 
 input = '(sqrt (/ 8 2))' // 2
-console.log(expressionEval(input) === 2)
+console.log(evaluate(input) === 2)
 
 input = '(* (/ 1 2) 3)' // 1.5
-console.log(expressionEval(input) === 1.5)
+console.log(evaluate(input) === 1.5)
 
 input = '(+ 1 (+ 2 3))' // 6
-console.log(expressionEval(input) === 6)
+console.log(evaluate(input) === 6)
 
 input = '( + ( + ( + 9 (+ 2 2)) 2) ( + 3 4) )' // 22
-console.log(expressionEval(input) === 22)
+console.log(evaluate(input) === 22)
 
 input = '(+ (+ 1 (- 1 1)) 1)' // 2
-console.log(expressionEval(input) === 2)
+console.log(evaluate(input) === 2)
 
 input = '(* 5 10)' // 50
-console.log(expressionEval(input) === 50)
+console.log(evaluate(input) === 50)
 
 // // _____________________________________if____________________________________________
 
-input = '( if (> 30 45) (+ 45 56) failedOutput)'
-console.log(expressionEval(input) === 'failedOutput')
+input = '( if (> 30 45) (+ 1 1) failedOutput)'
+console.log(evaluate(input) === 'failedOutput')
 
 input = '(if (= 12 12) (+ 78 2) 9)'
-console.log(expressionEval(input) === 80)
+console.log(evaluate(input) === 80)
 
 input = '(if #f 1 0)'
-console.log(expressionEval(input) === 0)
+console.log(evaluate(input) === 0)
+
+input = '(if #f 1 abc)'
+console.log(evaluate(input) === 'abc')
 
 // // _________________________________________________________________________________
+
