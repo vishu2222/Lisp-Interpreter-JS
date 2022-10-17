70eb6a530fe062ea796d1f127f0c56428c4a6108
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-10-17 22:51:42 +0530

    added define. define not Working. added ScopeS

--- lispEval.js
+++ lispEval.js
@@ -50,8 +50,8 @@ function booleanParser (input) { // anything that is not false or #f is true in 
   return null
 }
 
-// ifParser
-function ifParser (input) { // testCondition passCase failCase)
+// ifParser // (if <test> <consequent> <alternate>) another form // (if <test> <consequent>) 
+function ifParser (input, env) { // testCondition passCase failCase)
   let parsed = booleanParser(input)
   if (parsed === null) { console.log('can\'t parse testCondition'); return null }
   const testCondition = parsed[0]
@@ -76,15 +76,32 @@ function ifParser (input) { // testCondition passCase failCase)
   return [passCase, input]
 }
 
-// defineParser
-function defineParser (input) {
-  const env = Object.create(globalEnv)
+// defineParser // (define <variable> <expression>)
+function defineParser (input, env) {
+  let parsed = symbolParser(input)
+  if (parsed === null) { console.log('cannot parse symbol in define parser'); return null }
+  const key = parsed[0]
+  input = parsed[1]
+
+  if (input[0] === '(') {
+    const parsed = getListArg(input)
+    if (parsed === null) { console.log('cannot parse listArg in define parser'); return null }
+    input = parsed[1]
+    env[key] = expressionEval(parsed[0])
+  } else {
+    parsed = symbolParser(input)
+    if (parsed === null) { console.log('cannot parse atom in define parser'); return null }
+    env[key] = parsed[0]
+    input = parsed[1].trim()
+  }
+  return input
+  // console.log(input, '...')
 }
 
 // special forms
-function formParser (op, input) {
-  if (op === 'if') { return ifParser(input) }
-  if (op === 'define') { return defineParser(input) }
+function formParser (op, input, env) {
+  if (op === 'if') { return ifParser(input, env) }
+  if (op === 'define') { return defineParser(input, env) }
 }
 
 // returns first list expression argument from an input
@@ -129,7 +146,7 @@ const specialForms = ['if', 'define', 'quote']
 
 // expression evaluater
 function expressionEval (input, env = globalEnv) { // input = (op arg1 arg2 ...) arg is an atom or expression
-  if (input === null || input[0] !== '(') { return null }
+  if (input === null) { return null }
   input = input.slice(1).trim() // input = op arg1 arg2 ...)
 
   const parsed = symbolParser(input) // operators(including special forms) are symbols
@@ -138,13 +155,15 @@ function expressionEval (input, env = globalEnv) { // input = (op arg1 arg2 ...)
   input = parsed[1] // input = arg1, arg2,...)
 
   if (specialForms.includes(op)) { // if op belongs to (define, quote, if...)
-    const result = formParser(op, input)
-    if (result === null) { return null }
-    return result[0]
+    const parsed = formParser(op, input, env)
+    if (parsed === null || parsed === undefined) { return null }
+    return parsed[0]
   }
 
-  if (!Object.keys(env).includes(op)) { return null }
-  const args = getArgs(input) // getArgs('(arg1 arg2...)') = [exp1Val, exp2Val ...] // input = arg1, arg2,...)
+  const localScope = Object.keys(env)
+  const globalScope = Object.keys(Object.getPrototypeOf(env))
+  if (!localScope.includes(op) && !globalScope.includes(op)) { return null }
+  const args = getArgs(input, env) // getArgs('(arg1 arg2...)') = [exp1Val, exp2Val ...] // input = arg1, arg2,...)
   if (args === null) { console.log('err: invalid expression'); return null }
   return env[op](...args) // op([exp1Val, exp2Val ...])
 }
@@ -154,50 +173,51 @@ function evaluate (input) {
   input = input.replaceAll('(', '( ').replaceAll(')', ' )')
   console.log('input:', input)
   if (input[0] !== '(') { return null }
-  return expressionEval(input)
+  const env = Object.create(globalEnv)
+  return expressionEval(input, env)
 }
 
 let input
-input = '( define x (2 + 2))'
+input = '( define x (+ 2 2))'
 console.log(evaluate(input))
 
 // _____________________________________tests____________________________________________
 
 // // Math
 
-input = '(+ (+ 1 (- 1 1)) 2)' // 3
-console.log(evaluate(input) === 3)
+// input = '(+ (+ 1 (- 1 1)) 2)' // 3
+// console.log(evaluate(input) === 3)
 
-input = '(sqrt (/ 8 2))' // 2
-console.log(evaluate(input) === 2)
+// input = '(sqrt (/ 8 2))' // 2
+// console.log(evaluate(input) === 2)
 
-input = '(* (/ 1 2) 3)' // 1.5
-console.log(evaluate(input) === 1.5)
+// input = '(* (/ 1 2) 3)' // 1.5
+// console.log(evaluate(input) === 1.5)
 
-input = '(+ 1 (+ 2 3))' // 6
-console.log(evaluate(input) === 6)
+// input = '(+ 1 (+ 2 3))' // 6
+// console.log(evaluate(input) === 6)
 
-input = '( + ( + ( + 9 (+ 2 2)) 2) ( + 3 4) )' // 22
-console.log(evaluate(input) === 22)
+// input = '( + ( + ( + 9 (+ 2 2)) 2) ( + 3 4) )' // 22
+// console.log(evaluate(input) === 22)
 
-input = '(+ (+ 1 (- 1 1)) 1)' // 2
-console.log(evaluate(input) === 2)
+// input = '(+ (+ 1 (- 1 1)) 1)' // 2
+// console.log(evaluate(input) === 2)
 
-input = '(* 5 10)' // 50
-console.log(evaluate(input) === 50)
+// input = '(* 5 10)' // 50
+// console.log(evaluate(input) === 50)
 
 // // _____________________________________if____________________________________________
 
-input = '( if (> 30 45) (+ 1 1) failedOutput)'
-console.log(evaluate(input) === 'failedOutput')
+// input = '( if (> 30 45) (+ 1 1) failedOutput)'
+// console.log(evaluate(input) === 'failedOutput')
 
-input = '(if (= 12 12) (+ 78 2) 9)'
-console.log(evaluate(input) === 80)
+// input = '(if (= 12 12) (+ 78 2) 9)'
+// console.log(evaluate(input) === 80)
 
-input = '(if #f 1 0)'
-console.log(evaluate(input) === 0)
+// input = '(if #f 1 0)'
+// console.log(evaluate(input) === 0)
 
-input = '(if #t abc 1)'
-console.log(evaluate(input) === 'abc')
+// input = '(if #t abc 1)'
+// console.log(evaluate(input) === 'abc')
 
 // // _________________________________________________________________________________
