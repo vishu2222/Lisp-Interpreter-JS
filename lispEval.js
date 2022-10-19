d63099d92b2bc23507631dfbe818dbeb5466c914
vishu2222 <vishwanathreddyaenugu@gmail.com>
2022-10-19 22:38:19 +0530

    redone by converting parsers to evaluaters

--- lispEval.js
+++ lispEval.js
@@ -1,223 +1,186 @@
 
 const globalEnv = {
-  '+': (a, b) => a + b,
-  '-': (a, b) => a - b,
-  '*': (a, b) => a * b,
-  '/': (a, b) => a / b,
-  '>': (a, b) => a > b,
-  '<': (a, b) => a < b,
-  '>=': (a, b) => a >= b,
-  '<=': (a, b) => a <= b,
-  '=': (a, b) => a === b,
-  sqrt: (a) => Math.sqrt(a),
-  abs: (a) => Math.abs(a)
+  '+': (...args) => args.reduce((sum, i) => sum + i),
+  '-': (...args) => args.reduce((sum, i) => sum - i),
+  '*': (...args) => args.reduce((mul, i) => mul * i, 1),
+  '/': (...args) => { if (args.length === 2) { return args[0] / args[1] } return null },
+  '>': (...args) => args[0] > args[1],
+  '<': (...args) => args[0] < args[1],
+  '>=': (...args) => args[0] >= args[1],
+  '<=': (...args) => args[0] <= args[1],
+  '=': (...args) => args[0] === args[1],
+  '#t': true,
+  '#f': false,
+  true: true,
+  false: false,
+  sqrt: (...args) => Math.sqrt(args[0]),
+  abs: (...args) => Math.abs(args[0]),
+  pi: 3.14
+}
+const specialForms = ['if', 'define', 'quote', 'lambda']
+
+// numberEval
+function numberEval (num) { // add validation for num
+  if (Number(num) === null || isNaN(Number(num))) { return null }
+  return Number(num)
 }
 
-// numberParser
-function numberParser (input) {
-  const output = input.match(/^[-+]?([1-9]\d*|0)(\.\d+)?([eE][+-]?\d+)?/) // need to extend to complex range, a/b form , +|- nan.0, +|-inf.0
-  if (output) { return [Number(output[0]), input.slice(output[0].length).trim()] }
+// symbolEval
+function symbolEval (atom, env) { // add validation for symbol
+  if (Object.keys(env).includes(atom)) { return env[atom] }
   return null
 }
 
-// symbolParser
-function symbolParser (input) {
-  const output = input.match(/^[\w!$%&*/:<=?>~_^+-/*#]+[\w\d]*\s/) // need to verify for all cases
-  if (output) { return [output[0].trim(), input.slice(output[0].length).trim()] }
+// stringParser
+function stringEval (input) {
+  const mached = input.match(/".+"/) // [\w!$%&*/:<=?>~_^+-/*#]+[\w\d]*
+  if (mached) { return mached[0].trim() }
   return null
 }
 
-// atomParser
-function atomParser (input) {
-  return numberParser(input) || symbolParser(input) // an atom is a number or symbol
+// atomEval // potential errors when evaluaters return null or false in ||
+function atomEval (input, env) {
+  if (numberEval(input) === 0) { return 0 }
+  if (symbolEval(input, env) === false) { return false }
+  return numberEval(input) || symbolEval(input, env) || stringEval(input)
 }
 
-// booleanParser
-function booleanParser (input) { // anything that is not false or #f is true in scheme
+// getExpression
+function getExpression (input) {
+  input = input.trim()
   if (input[0] !== '(') {
-    const temp = atomParser(input)
-    return [temp[0], temp[1]]
+    const item = input.split(' ')[0]
+    return [item, input.slice(item.length).trim()]
   }
-  if (input[0] === '(') {
-    const temp = getListArg(input)
-    const testArg = temp[0]
-    input = temp[1].trim()
-    const parsed = expressionEval(testArg)
-    if (parsed === false) { return [false, input] }
-    else { return [true, input] }
+  let str = input.slice(1)
+  let braceCount = 1
+  let iterCount = 0
+  while (braceCount !== 0) {
+    iterCount++
+    if (iterCount > input.length) { return null }
+    if (str[0] === '(') { braceCount++; str = str.slice(1) } else if (str[0] === ')') { braceCount--; str = str.slice(1) } else { str = str.slice(1) }
   }
-  console.log('invalid test consdition in if')
-  return null
+  return [input.slice(0, input.length - str.length).trim(), str.trim()]
 }
 
-// ifParser // (if <test> <consequent> <alternate>) another form // (if <test> <consequent>) 
-function ifParser (input, env) { // testCondition passCase failCase)
-  let parsed = booleanParser(input)
-  if (parsed === null) { console.log('can\'t parse testCondition'); return null }
-  const testCondition = parsed[0]
-  input = parsed[1].trim()
-
-  parsed = getListArg(input) || atomParser(input)
-  if (parsed === null) { return null }
-  const passCase = parsed[0]
-  input = parsed[1]
-
-  parsed = getListArg(input) || atomParser(input)
-  if (parsed === null) { return null }
-  const failCase = parsed[0]
-  input = parsed[1]
-
-  if (testCondition === false || testCondition === '#f') { //  || testCondition === '#f'
-    if (failCase[0] === '(') { return [expressionEval(failCase), input] }
-    return [failCase, input]
+// getArgs
+function getArgs (input) {
+  const args = []
+  while (input[0] !== ')') {
+    const parsedExp = getExpression(input)
+    args.push(parsedExp[0])
+    input = parsedExp[1]
   }
+  return args
+}
 
-  if (passCase[0] === '(') { return [expressionEval(passCase), input] }
-  return [passCase, input]
+// ifParser (if <test> <consequent> <alternate>)
+function ifParser (input, env) {
+  let parsedExp = getExpression(input)
+  const testArg = parsedExp[0]
+  input = parsedExp[1]
+
+  parsedExp = getExpression(input)
+  const passArg = parsedExp[0]
+  input = parsedExp[1]
+
+  parsedExp = getExpression(input)
+  const failArg = parsedExp[0]
+  input = parsedExp[1]
+
+  if (expressionEval(testArg, env) === false) { return expressionEval(failArg, env) }
+  return expressionEval(passArg, env)
 }
 
 // defineParser // (define <variable> <expression>)
 function defineParser (input, env) {
-  let parsed = symbolParser(input)
-  if (parsed === null) { console.log('cannot parse symbol in define parser'); return null }
-  const key = parsed[0]
-  input = parsed[1]
-
-  if (input[0] === '(') {
-    const parsed = getListArg(input)
-    if (parsed === null) { console.log('cannot parse listArg in define parser'); return null }
-    input = parsed[1]
-    env[key] = expressionEval(parsed[0])
-  } else {
-    parsed = symbolParser(input)
-    if (parsed === null) { console.log('cannot parse atom in define parser'); return null }
-    env[key] = parsed[0]
-    input = parsed[1].trim()
-  }
-  return input
-  // console.log(input, '...')
+  console.log(input)
 }
 
 // special forms
 function formParser (op, input, env) {
   if (op === 'if') { return ifParser(input, env) }
   if (op === 'define') { return defineParser(input, env) }
+  return null
 }
 
-// returns first list expression argument from an input
-function getListArg (input) { // input = (getThisArg) ()...())
-  if (input[0] !== '(') { return null }
-  let count = 1
-  let str = input.slice(1) // str = getThisArg) ()...()) , count = 1 // getThisArg is anything between ( and a matching )
-  while (count !== 0) { // case when count !== 0?
-    if (str[0] === '(') {
-      count++
-      str = str.slice(1)
-    } else if (str[0] === ')') {
-      count--
-      str = str.slice(1)
-    } else { str = str.slice(1) }
+// compoundExpEval
+function compoundExpEval (input, env) {
+  input = input.slice(1)
+  const parsedExp = getExpression(input)
+  if (parsedExp === null) { return null }
+  let op
+  [op, input] = [parsedExp[0], parsedExp[1]]
+  if (specialForms.includes(op)) {
+    return formParser(op, input, env)
   }
-  return [input.slice(0, input.length - str.length), str.trim()] // returns [listArg, rest of the string] // change to returning evaluated exp?
-}
-
-// get atom args and expression args
-function getArgs (input) { // input = arg1, arg2,..) args are either atom or list expressions
-  const args = []
-  while (input[0] !== ')') { // i.e is while the last ')' in the input is not reached
-    if (input[0] === '(') { // if the arg is a list expression
-      const ParsedList = getListArg(input) //
-      if (ParsedList === null) { return null }
-      input = ParsedList[1].trim()
-      const arg = expressionEval(ParsedList[0])
-      if (arg === null) { return null }
-      args.push(arg)
-    } else { // get atom expression
-      const parsed = atomParser(input)
-      if (parsed === null) { return null }
-      args.push(parsed[0])
-      input = parsed[1]
-    }
+  if (Object.keys(env).includes(op)) {
+    const parsedArgs = getArgs(input)
+    const args = []
+    parsedArgs.forEach(arg => { args.push(expressionEval(arg, env)) })
+    return env[op](...args)
   }
-  return args
 }
 
-const specialForms = ['if', 'define', 'quote']
-
-// expression evaluater
-function expressionEval (input, env = globalEnv) { // input = (op arg1 arg2 ...) arg is an atom or expression
-  if (input === null) { return null }
-  input = input.slice(1).trim() // input = op arg1 arg2 ...)
-
-  const parsed = symbolParser(input) // operators(including special forms) are symbols
-  if (parsed === null) { console.log('error parsing op'); return null } // not a valid operator
-  const op = parsed[0]
-  input = parsed[1] // input = arg1, arg2,...)
-
-  if (specialForms.includes(op)) { // if op belongs to (define, quote, if...)
-    const parsed = formParser(op, input, env)
-    if (parsed === null || parsed === undefined) { return null }
-    return parsed[0]
-  }
-
-  const localScope = Object.keys(env)
-  const globalScope = Object.keys(Object.getPrototypeOf(env))
-  if (!localScope.includes(op) && !globalScope.includes(op)) { return null }
-  const args = getArgs(input, env) // getArgs('(arg1 arg2...)') = [exp1Val, exp2Val ...] // input = arg1, arg2,...)
-  if (args === null) { console.log('err: invalid expression'); return null }
-  return env[op](...args) // op([exp1Val, exp2Val ...])
+// expressionEval
+function expressionEval (expression, env) {
+  if (expression[0] === '(') { return compoundExpEval(expression, env) }
+  return atomEval(expression, env)
 }
 
-// evaluate input
-function evaluate (input) {
-  input = input.replaceAll('(', '( ').replaceAll(')', ' )')
-  console.log('input:', input)
-  if (input[0] !== '(') { return null }
-  const env = Object.create(globalEnv)
-  return expressionEval(input, env)
+// main
+function main (input) {
+  console.log('Given input:', input, '\n')
+  input = input.replaceAll('(', '( ').replaceAll(')', ' )') // * fix 'atom)' in getExpression()
+  return expressionEval(input, globalEnv)
 }
 
-let input
-input = '( define x (+ 2 2))'
-console.log(evaluate(input))
+const input = '(define x (+ 5 5))'
+console.log(main(input))
 
-// _____________________________________tests____________________________________________
+// console.log(Object.keys(globalEnv).includes('#f'))
+// ______________________________Math Cases_______________________________
+// let input
 
-// // Math
+// input = '-5 '
+// console.log(main(input) === -5)
 
-// input = '(+ (+ 1 (- 1 1)) 2)' // 3
-// console.log(evaluate(input) === 3)
+// input = 'pi'
+// console.log(main(input) === 3.14)
+
+// input = '-5'
+// console.log(main(input) === -5)
 
 // input = '(sqrt (/ 8 2))' // 2
-// console.log(evaluate(input) === 2)
+// console.log(main(input) === 2)
 
 // input = '(* (/ 1 2) 3)' // 1.5
-// console.log(evaluate(input) === 1.5)
+// console.log(main(input) === 1.5)
 
 // input = '(+ 1 (+ 2 3))' // 6
-// console.log(evaluate(input) === 6)
+// console.log(main(input) === 6)
 
 // input = '( + ( + ( + 9 (+ 2 2)) 2) ( + 3 4) )' // 22
-// console.log(evaluate(input) === 22)
+// console.log(main(input) === 22)
 
 // input = '(+ (+ 1 (- 1 1)) 1)' // 2
-// console.log(evaluate(input) === 2)
+// console.log(main(input) === 2)
 
 // input = '(* 5 10)' // 50
-// console.log(evaluate(input) === 50)
+// console.log(main(input) === 50)
+// _____________________________________if______________________________________
 
-// // _____________________________________if____________________________________________
-
-// input = '( if (> 30 45) (+ 1 1) failedOutput)'
-// console.log(evaluate(input) === 'failedOutput')
+// let input
+// input = '( if (> 30 45) (+ 1 1) "failedOutput")'
+// console.log(main(input) === '"failedOutput"')
 
 // input = '(if (= 12 12) (+ 78 2) 9)'
-// console.log(evaluate(input) === 80)
+// console.log(main(input) === 80)
 
 // input = '(if #f 1 0)'
-// console.log(evaluate(input) === 0)
+// console.log(main(input) === 0)
 
-// input = '(if #t abc 1)'
-// console.log(evaluate(input) === 'abc')
+// input = '(if #t "abc" 1)'
+// console.log(main(input) === '"abc"')
 
-// // _________________________________________________________________________________
+// ______________________________________________________________________
